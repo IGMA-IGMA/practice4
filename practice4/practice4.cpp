c68f5e5171750a6cb2a63c31fc492ed561005f51
IGMA-IGMA <i9296517650@yandex.ru>
2025-09-28 23:03:17 +0300

    3

--- practice4/practice4.cpp
+++ practice4/practice4.cpp
@@ -7,11 +7,9 @@ int main()
 {
     float x, y;
     cin >> x >> y;
-
     if (x == 0 || x <= 0) {
         return 1;
     }
     cout << max(1.0 / cos(y * y / x), pow(x + log(1.0 / x), 1.0 / 3.0));
-
     return 0;
 }
